517e3611fc6ab5abf8afa40aa5a0ce2fb2932c27
adrtara <tarantinoadriano2605@gmail.com>
2025-06-26 18:38:36 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -110,7 +110,7 @@ function App() {
           
           <div className="relative h-full flex flex-col justify-center items-end pr-32 text-white">
             <div className="text-center">
-              <h2 className="text-7xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">SELL</h2>
+              <h2 className="text-7xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">VENDI</h2>
             </div>
           </div>
         </div>
