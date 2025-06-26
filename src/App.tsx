8837d12bdb2a90f7b478be8a12b12e356c665225
adrtara <tarantinoadriano2605@gmail.com>
2025-06-26 20:14:17 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
                 Home
               </a>
               <a href="#about" className="text-slate-600 hover:text-slate-900 font-medium transition-colors duration-200 flex items-center">
-                About <img src={logoImage} alt="Logo" className="inline-block h-10 ml-1 align-middle" />
+                About <img src={logoImage} alt="Logo" className="inline-block h-9 ml-1 align-middle" />
               </a>
               <a href="#workflow" className="text-slate-600 hover:text-slate-900 font-medium transition-colors duration-200">
                Il Metodo
