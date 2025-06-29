ff6a460fbd5440149b289a4cd8e254fbd10406a9
adrtara <tarantinoadriano2605@gmail.com>
2025-06-29 23:38:56 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -111,7 +111,7 @@ function App() {
           
           <div className="relative h-full flex flex-col justify-center items-end pr-32 text-white">
             <div className="text-center">
-              <h2 className="text-7xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">VENDI</h2>
+              <h2 className="text-6xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">VENDI</h2>
             </div>
           </div>
         </div>
@@ -145,7 +145,7 @@ function App() {
           
           <div className="relative h-full flex flex-col justify-center items-start pl-32 text-white">
             <div className="text-center">
-              <h2 className="text-7xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">ACQUISTA</h2>
+              <h2 className="text-6xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">ACQUISTA</h2>
             </div>
           </div>
         </div>
