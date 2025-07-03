fe6091753c6f82906b89c0ab7b96394732907061
adrtara <tarantinoadriano2605@gmail.com>
2025-07-04 00:11:35 +0200

    Add Google Fonts to HTML and Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     extend: {
       fontFamily: {
         'dm-serif-text': ['DM Serif Text', 'serif'],
+        'cal-sans': ['Cal Sans', 'sans-serif'],
+        'fredoka': ['Fredoka', 'sans-serif'],
+        'gantari': ['Gantari', 'sans-serif'],
+        'markazi-text': ['Markazi Text', 'serif'],
+        'prompt': ['Prompt', 'sans-serif'],
+        'zain': ['Zain', 'sans-serif'],
       },
     },
   },
