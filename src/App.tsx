f95ecc3b94e9a710fd13e36e14591e3d9120062e
adrtara <tarantinoadriano2605@gmail.com>
2025-07-07 23:50:24 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
                     : 'border-transparent'
                 }`}
               >
-                About <img src={logoImage} alt="Logo" className="inline-block h-4 ml-1 align-middle" />
+                About <img src={logoImage} alt="Logo" className="inline-block h-4 ml-2 align-middle" />
               </a>
               <a 
                 href="#workflow" 
