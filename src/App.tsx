5ee09a2dbd3a1cb763f93d0a09c39012bd61e6de
adrtara <tarantinoadriano2605@gmail.com>
2025-07-08 23:54:54 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -215,7 +215,7 @@ function App() {
           <div className="relative h-full flex flex-col justify-center items-center md:items-start pl-0 md:pl-32 text-white">
             <div className="text-center md:text-left">
               <h2 className="text-4xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">VOSPRA <br /> STUDIO</h2>
-              <p className="text-xl font-dm-serif-text mt-2 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-500">La tua visione, la nostra guida</p>
+              <p className="text-xl font-dm-serif-text mt-2 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-500">Soluzioni strategiche per business digitali ambiziosi.</p>
             </div>
           </div>
         </div>
