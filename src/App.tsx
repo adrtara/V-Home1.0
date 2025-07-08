f898598cbdba617c831a88985580cc3b555c6c36
adrtara <tarantinoadriano2605@gmail.com>
2025-07-08 20:22:52 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -214,7 +214,7 @@ function App() {
           
           <div className="relative h-full flex flex-col justify-center items-center md:items-start pl-0 md:pl-32 text-white">
             <div className="text-center">
-              <h2 className="text-4xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">STRATEGY</h2>
+              <h2 className="text-4xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90">CONSULENZA</h2>
             </div>
           </div>
         </div>
