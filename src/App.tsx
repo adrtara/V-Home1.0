573cae0215cd6676f9650752f6b34ed7de2fff51
adrtara <tarantinoadriano2605@gmail.com>
2025-07-09 21:55:55 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -177,7 +177,7 @@ function App() {
           />
           <div className="absolute inset-0 bg-gradient-to-br from-slate-900/80 via-slate-800/70 to-slate-700/50" />
           
-          <div className="relative h-full flex flex-col justify-center md:items-end pr-0 md:pr-24 text-white">
+          <div className="relative h-full flex flex-col justify-center md:items-end pr-0 md:pr-26 text-white">
             <div className="text-center md:text-right">
               <h2 className="text-4xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90 text-center">MARKET</h2>
               <p className="text-xl font-dm-serif-text mt-2 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-500 text-center">La vetrina selettiva per acquistare <br /> o vendere valore.</p>
@@ -212,7 +212,7 @@ function App() {
           />
           <div className="absolute inset-0 bg-gradient-to-bl from-slate-900/80 via-slate-800/70 to-slate-700/50" />
           
-          <div className="relative h-full flex flex-col justify-center md:items-start pl-0 md:pl-24 text-white">
+          <div className="relative h-full flex flex-col justify-center md:items-start pl-0 md:pl-26 text-white">
             <div className="text-center md:text-left">
               <h2 className="text-4xl font-dm-serif-text tracking-wider transition-transform duration-1000 ease-out group-hover:scale-90 text-center">VOSPRA <br /> STUDIO</h2>
               <p className="text-xl font-dm-serif-text mt-2 opacity-100 md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-500 text-center">Soluzioni strategiche per business<br />digitali ambiziosi.</p>
