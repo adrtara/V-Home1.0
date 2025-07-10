7fbe29657c592fc4dcffa77d2152f364b4c5c257
adrtara <tarantinoadriano2605@gmail.com>
2025-07-11 00:05:47 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
 
       {/* Navigation Bar - Full width on mobile, with lateral margins on desktop */}
     <Navbar 
-    currentPath="/#Home "
+    currentPath="/"
     isMobileMenuOpen={isMobileMenuOpen}
     toggleMobileMenu={toggleMobileMenu}
     handleNavClick={handleNavClick}
