c403155ad49bc761d9bd7986fa10bbe2fd930671
adrtara <tarantinoadriano2605@gmail.com>
2025-07-24 11:45:10 +0200

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ export default {
       fontFamily: {
         // Font personalizzati
         'dm-serif-text': ['DM Serif Text', 'serif'],
-        'cal-sans': ['Cal Sans', 'sans-serif'],
-        'fredoka': ['Fredoka', 'sans-serif'],
-        'gantari': ['Gantari', 'sans-serif'],
-        'markazi-text': ['Markazi Text', 'serif'],
-        'prompt': ['Prompt', 'sans-serif'],
         'zain': ['Zain', 'sans-serif'],
         
         // Override dei font di default per renderli più sottili
