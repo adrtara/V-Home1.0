288dda7efe3f6f50d319e36d8ff024d5f28e97fb
adrtara <tarantinoadriano2605@gmail.com>
2025-07-24 11:28:11 +0200

    Updated Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({
           <div className="flex items-center justify-between h-24 lg:grid lg:grid-cols-3">
             {/* Logo */}
             <div className="text-[28px] font-zain text-slate-800 tracking-wide flex items-center">
-              <img src="https://i.postimg.cc/BnjzvK3Z/Logo-Senza-Sfondo.png" alt="V Logo" className="inline-block h-6 -translate-y-[6px] -mr-1 relative" />
+              <img src="https://i.postimg.cc/BnjzvK3Z/Logo-Senza-Sfondo.png" alt="V Logo" className="inline-block h-6 -translate-y-[7px] -mr-1 relative" />
               <span> 
                 ospra.
               </span>
